f28973bf17d66170dbf68dbbee86fde9d3e1c998
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-11-06 18:48:39 +0800

    lesson4a: arrays

---
+++ learn-go/lesson4a_arrays/sum.go
@@ -0,0 +1,19 @@
+package main
+
+// By standard for loop
+// func Sum(numbers [5]int) int {
+// 	sum := 0
+// 	for i := 0; i < 5; i++ {
+// 		sum += numbers[i]
+// 	}
+// 	return sum
+// }
+
+// By range
+func Sum(numbers [5]int) int {
+	sum := 0
+	for _, number := range numbers{
+		sum += number
+	}
+	return sum
+}
